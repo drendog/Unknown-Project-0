542347b7645681e9ac3a746901deeb42397d77c2
drendog <graziano27dg@gmail.com>
2020-06-12 16:10:39 +0200

    Add missing tooltip

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -15,6 +15,7 @@
               <b-button
                 pill
                 :pressed.sync="$store.state.isVolumeRandom"
+                title="Random sounds volume"
                 class="btn-xs"
                 variant="outline-teal"
               >
